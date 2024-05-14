1dc9718df8888f844181d193b5e8e70f4d75f4a8
GuillaumeCld <guillaume.coulaud33@orange.fr>
2024-05-14 23:04:55 +0200

    Adding pavament plot for interval MP

--- interval_pavement.py
+++ interval_pavement.py
@@ -1,46 +1,91 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
+import pandas as pd
 
 
-n = 100
-width  = 10
-height = 60
+df = pd.read_csv("Data/era5_daily_series_sst_60S-60N_ocean.csv", sep=",", skiprows=18)
+df = df[df["status"] == "FINAL"]
+df = df[(df['date'] >= '1979-01-01') & (df['date'] <= '2023-12-31')]
+ts_date = df["date"].values.astype(np.datetime64)
 
-fig = plt.figure(figsize=(width, height))
+list_ind = []
+
+for year in range(1979, 2024):
+    list_ind.append(np.where(ts_date == np.datetime64(str(year) + "-01-01"))[0][0])
+
+
+I = 90
+window_size = 14
+n = ts_date.shape[0]
+n_sequence = n - window_size + 1
+
+
+
+if window_size > I//2:
+    nb_block = len(list_ind)
+else:
+    nb_block = len(list_ind) + 1
+
+fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal')
 plt.ylim(max(plt.ylim()), min(plt.ylim()))
 
-metarows = n // height 
-if n % height > 0:
-    metarows += 1
+for col in list_ind:
+    plt.vlines(col, 0, n_sequence, color='purple', linestyles='dashed', alpha=0.3)
 
-nb_block = n // width
-if n % width > 0:
-    nb_block += 1
 
-def_height = height
+metarows = len(list_ind)
 
-missing = int(np.ceil((n  - (nb_block - int(np.ceil(height / width)) )*width)/width))
+
+period_witdh = 0
+
+width = I
 for row in range(metarows):
-    block_shift = (row+1) * height
-    diagonal_shift = int(np.ceil(block_shift / width))
-    for block in range(nb_block+missing):
-        x1 = -diagonal_shift * width + row*height + block * width
-        x2 = x1 + width
-        x3 = x1 + height
-        x4 = x1 + height + width
+    if row == metarows - 1:
+        height = n_sequence - list_ind[row]
+    else:
+        height = list_ind[row+1] - list_ind[row]
 
-        y1 = row*def_height
+    for col in range(nb_block):
+
+        if col < len(list_ind):
+            if col == len(list_ind) - 1:
+                width = np.minimum(I, n_sequence - (list_ind[col]- I//2))
+                period_witdh = n+1 - list_ind[col]
+            else:
+                width = I
+                period_witdh = list_ind[col+1] - list_ind[col]
+            x1 = list_ind[col] - I//2
+        else:
+            x1 = n+1 - I//2  
+            period_witdh = -1
+            if x1 >= n_sequence:
+                break
+
+        x2 = x1 + width
+        x3 = x2 + height
+        x4 = x1 + height
+
+        y1 = list_ind[row]
         y2 = y1
         y3 = y1+height
         y4 = y3
+        x = [x1, x2, x3, x4]
+        y = [y1, y2, y3, y4]
 
-        x = [x1, x2, x4, x3]
-        y = [y1, y2, y4, y3]
+        ax.add_patch(patches.Polygon(xy=list(zip(x,y)), fill=True, color='blue', alpha=0.5))
 
-        ax.add_patch(patches.Polygon(xy=list(zip(x,y)), fill=False))
+        if period_witdh > 0:
+            if height > period_witdh:
+                plt.hlines(y3, x4-1, x4, color='orange', alpha=1, lw=20)
+                plt.hlines(y3, x3-1, x3, color='red', alpha=1, lw=20)
+            elif height < period_witdh:
+                plt.hlines(y3, x4, x4+1, color='red', alpha=1, lw=20)
+                plt.hlines(y3, x3, x3+1, color='green', alpha=1, lw=20)
 
-plt.plot([0, n, n, 0, 0], [0, 0, n, n, 0], 'k-', color='red')
+
+plt.plot([0, n, n, 0, 0], [0, 0, n, n, 0], 'k-', color='yellow')
+plt.plot([0, n_sequence, n_sequence, 0, 0], [0, 0, n_sequence, n_sequence, 0], 'k-', color='red')
 plt.axis('equal')
 plt.show()
